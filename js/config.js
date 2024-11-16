ec6606509b40d35f84a3311d98f56a91789576d8
Diego-Murcia <dmurcia.cespedes@gmail.com>
2024-11-16 09:39:58 -0500

    feat: config cognito integration file

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_9nOqOOnrh', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7o3fi8gr16fa7q0fjdd7jjh5ur', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_9nOqOOnrh/.well-known/jwks.json' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
